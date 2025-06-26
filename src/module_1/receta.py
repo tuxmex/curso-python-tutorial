32391e6eaf8d4a57b462ed24f926b82e1bc0aafb
tuxmex <anastaciorodriguez@utng.edu.mx>
2025-06-26 16:25:37 -0600

    Add recipe for Tacos de Programador to receta.py

---
+++ src/module_1/receta.py
@@ -0,0 +1,10 @@
+# Mi primera receta: Tacos de Programdor
+print("¡Bienvenido a la receta de Tacos de Programador!")
+print("Paso 1: Abrir el editor de código. (como abrir la despensa)")
+print("Paso 2: Escribir el código limpio. (como lavar los trastes)")
+print("Paso 3: Declarar las variables. (como sacar los ingredientes)")
+print("Paso 4: Usar Funciones. (como revorlver los ingredientes)")
+print("Paso 5: Ejecutar el código. (como cocinar los tacos)")
+print("¡Disfruta de tus Tacos de Programador!")
+# Fin de la receta
+print("Recuerda: ¡Siempre depura antes de servir!")
